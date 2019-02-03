207174e42c18aeffffee214c8104a9c23d5ff377
Wandalen <wandalen.me@gmail.com>
2019-02-03 21:26:04 +0200

    auto commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # wAbstractGraph [![Build Status](https://travis-ci.org/Wandalen/wAbstractGraph.svg?branch=master)](https://travis-ci.org/Wandalen/wAbstractGraph) [![Build Status](https://ci.appveyor.com/api/projects/status/github/Wandalen/wabstractgraph)](https://ci.appveyor.com/project/Wandalen/wabstractgraph)
 
-___
+Collection of abstract data structures and algorithms to process graphs. The module does not bound to any specific format of a graph, so providing adapters toy may use it with anyone. It implements depth-first search, breadth-first search, extracting strongly connected components, topological sort, shortest path search, and others.
 
 ## Try out
 ```
--- sample/Sample.js
+++ sample/Sample.js
@@ -1,7 +1,48 @@
 
-let _ = require( 'wTools' );
+let _ = require( '..' );
 
 /**/
 
-...
+var a = { name : 'a', nodes : [] } // 1
+var b = { name : 'b', nodes : [] } // 2
+var c = { name : 'c', nodes : [] } // 3
+var d = { name : 'd', nodes : [] } // 4
+var e = { name : 'e', nodes : [] } // 5
+var f = { name : 'f', nodes : [] } // 6
+var g = { name : 'g', nodes : [] } // 7
+var h = { name : 'h', nodes : [] } // 8
+var i = { name : 'i', nodes : [] } // 9
+var j = { name : 'j', nodes : [] } // 10
 
+a.nodes.push( b ); // 1
+b.nodes.push( e, f ); // 2
+c.nodes.push( b ); // 3
+d.nodes.push( a, g ); // 4
+e.nodes.push( a, c, h ); // 5
+f.nodes.push(); // 6
+g.nodes.push( h ); // 7
+h.nodes.push( i ); // 8
+i.nodes.push( f, h ); // 9
+j.nodes.push(); // 10
+
+var sys = new _.AbstractGraphSystem();
+var group = sys.groupMake();
+
+group.nodesAdd([ a, b, c, d, e, f, g, h, i, j ]);
+logger.log( group.exportInfo() );
+
+/*
+xxx
+*/
+
+var layers = group.topologicalSortSourceBasedBfs();
+
+/*
+[
+  [ 'd', 'j' ],
+  [ 'a', 'g' ],
+  [ 'b', 'h' ],
+  [ 'e', 'f', 'i' ],
+  [ 'c' ]
+];
+*/
