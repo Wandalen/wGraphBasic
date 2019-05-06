7060ac0565f5a837a5b3fce6833effb1f391f731
Wandalen <wandalen.me@gmail.com>
2019-05-06 11:32:51 +0400

    auto commit

---
+++ out/debug/dwtools/amid/agraph/UseAbstractBase.s
@@ -0,0 +1,32 @@
+( function _UseAbstractBase_s_( ) {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  let _ = require( '../../Tools.s' );
+
+  _.include( 'wProto' );
+  _.include( 'wCopyable' );
+
+}
+
+//
+
+/**
+ * @summary Collection of abstract data structures and algorithms to process graphs.
+ * @namespace "wTools.graph"
+ * @mebmerof module:Tools/mid/AbstractGraphs
+*/
+
+var _ = _global_.wTools;
+var Parent = null;
+var Self = _global_.wTools.graph = _global_.wTools.graph || Object.create( null );
+
+//
+
+if( typeof module !== 'undefined' && module !== null )
+module[ 'exports' ] = Self;
+
+})();
