20f6076e0b58c3d256f69979e84eea7cda9cad52
Wandalen <wandalen.me@gmail.com>
2019-10-03 21:13:59 +0400

    auto commit

---
+++ proto/dwtools/amid/l1/graphBasic/l1/Abstract.s
@@ -0,0 +1,24 @@
+( function _Abstract_s_( ) {
+
+'use strict';
+
+//
+
+var _ = _global_.wTools;
+var Parent = null;
+var Self = _global_.wTools.graph;
+_.assert( !!Self );
+
+// --
+// declare
+// --
+
+var Extend =
+{
+}
+
+//
+
+_.mapExtend( Self, Extend );
+
+})();
